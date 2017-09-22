ea33b16d2e1cefb370040f02891d91fb4cf184a3
Sarah <sarahraqueld@gmail.com>
2017-09-22 11:22:30 -0300

    Added a Procfile

--- movieit/settings.py
+++ movieit/settings.py
@@ -47,13 +47,13 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAdminUser',
     ],
-    '''
+    
     'DEFAULT_RENDERER_CLASSES': (
         'rest_framework.renderers.JSONRenderer',
     ),
     'DEFAULT_PARSER_CLASSES': (
         'rest_framework.parsers.JSONParser',
-    ),'''
+    ),
     'PAGE_SIZE': 10
 }
 
